839c476896324da0c38edaea0384215f8c8515b2
Nicholas Evans <nevans85.pro@gmail.com>
2017-10-17 20:40:47 -0700

    write p03 and p04

--- lru_cache/lib/p03_hash_set.rb
+++ lru_cache/lib/p03_hash_set.rb
@@ -9,18 +9,29 @@ class HashSet
   end
 
   def insert(key)
+    unless include?(key)
+      hash = key.hash
+      self[hash] << key
+      @count += 1
+      resize! if @count == num_buckets
+    end
   end
 
   def include?(key)
+    hash = key.hash
+    self[hash].include?(key)
   end
 
   def remove(key)
+    hash = key.hash
+    self[hash].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +39,9 @@ class HashSet
   end
 
   def resize!
+    elements = @store.flatten
+    @store = Array.new(num_buckets * 2) { [] }
+    @count = 0
+    elements.each { |el| insert(el) }
   end
 end
--- lru_cache/lib/p04_linked_list.rb
+++ lru_cache/lib/p04_linked_list.rb
@@ -15,11 +15,19 @@ class Node
   def remove
     # optional but useful, connects previous node to next node
     # and removes self from list.
+    @prev.next = @next
+    @next.prev = @prev
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,34 +36,57 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each { |node| return node.val if node.key == key }
   end
 
   def include?(key)
+    each { |node| return true if node.key == key }
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key,val)
+    new_node.prev = last
+    new_node.next = @tail
+    last.next = new_node
+    @tail.prev = new_node
   end
 
   def update(key, val)
+    each do |node|
+      if node.key == key
+        node.val = val
+        break
+      end
+    end
   end
 
   def remove(key)
+    each { |node| node.remove if node.key == key }
   end
 
-  def each
+  def each(&prc)
+    current_node = @head.next
+    until current_node == @tail
+      yield(current_node)
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
+  end
 end
