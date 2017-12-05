696eb365d1622bb538623c3ad2c25b808490e3b5
Nicholas Evans <nevans85.pro@gmail.com>
2017-12-05 09:07:15 -0800

    add linked lists - print elements

---
+++ hacker_rank/data_structures/linked_lists/print_elements.py
@@ -0,0 +1,21 @@
+# prompt: https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem
+
+"""
+ Print elements of a linked list on console
+ head input could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+ 
+ 
+"""
+def print_list(head):
+    curr_node = head
+    while curr_node.next:
+        print (curr_node.data)
+        curr_node = curr_node.next
+    print(curr_node.data)
