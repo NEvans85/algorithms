27fbb2537fe5b32730762494c2920ef1fc0d1784
Nicholas Evans <nevans85.pro@gmail.com>
2017-12-05 09:18:07 -0800

    add append node

---
+++ hacker_rank/data_structures/linked_lists/insert_node_at_tail.py
@@ -0,0 +1,25 @@
+# prompt: https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem
+
+"""
+ Insert Node at the end of a linked list 
+ head pointer input could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+ 
+ return back the head of the linked list in the below method
+"""
+
+def Insert(head, data):
+    if head:
+        node = head
+        while node.next:
+            node = node.next
+        node.next = Node(data)
+        return head
+    else:
+        return Node(data)
