e0dec606f4981eebc60c8fbfa0f207d0a9dc33b7
Nicholas Evans <nevans85.pro@gmail.com>
2017-12-18 09:07:07 -0800

    finish largest rectangle problem

--- hacker_rank/data_structures/stacks/largest_rectangle.py
+++ hacker_rank/data_structures/stacks/largest_rectangle.py
@@ -6,13 +6,20 @@ def largestRectangle(h):
     posStack = [0]
     hStack = [h[0]]
     maxSize = 0
-    for idx in range(1, len(h)):
-        if h[idx] > h[posStack[-1]]:
+    idx = 1
+    while idx < len(h):
+        if len(hStack) == 0 or h[idx] > hStack[-1]:
             posStack.append(idx)
             hStack.append(h[idx])
-        elif h[idx] < h[posStack[-1]]:
-            maxSize = max([maxSize, hStack.pop() * (idx - positions[-1])])
+        elif h[idx] < hStack[-1]:
+            while len(hStack) > 0 and h[idx] < hStack[-1]:
+                tempPos = posStack.pop()
+                maxSize = max([maxSize, hStack.pop() * (idx - tempPos)])
             hStack.append(h[idx])
+            posStack.append(tempPos)
+        idx += 1
+    while len(hStack) > 0:
+        maxSize = max([maxSize, hStack.pop() * (idx - posStack.pop())])
     return maxSize
 
 if __name__ == "__main__":
